8804f2ad7f799bbe34c50bce7d3bd040bd3fbac9
not-inept <mason@notinept.me>
2017-12-04 11:07:24 -0500

    Fin module now does what it is supposed to-- returns findata struct

--- src/finance.rs
+++ src/finance.rs
@@ -6,129 +6,134 @@ extern crate hyper_tls;
 
 use std::io;
 use futures::{Future, Stream};
-use hyper::Client;
 use tokio_core::reactor::Core;
 use serde_json::Value;
 
+// A struct for holding financial data-- makes everything easier to work with
 pub struct FinanceData {
-	open : f64,
-	close : f64,
-	current : f64,
-	hr_low : f64,
-	hr_high : f64
+    time: String,
+    open: f64,
+    close: f64,
+    current: f64,
+    hr_low: f64,
+    hr_high: f64,
+    hr_volume: f64,
 }
-
+impl Default for FinanceData {
+    fn default() -> FinanceData {
+        FinanceData {
+            time: String::from("0"),
+            open: 1.0,
+            close: 1.0,
+            current: 1.0,
+            hr_low: 1.0,
+            hr_high: 1.0,
+            hr_volume: 1.0,
+        }
+    }
+}
 // Fetch financial data in the form of findata struct for storing
-pub fn fetch(api_key : String) -> FinanceData {
-	return FinanceData {
-		open : 1.0,
-		close : 1.0,
-		current : 1.0,
-		hr_low : 1.0,
-		hr_high : 1.0		
-	}
-}
+pub fn fetch(api_key: String) -> FinanceData {
+    // Build client for HTTPS Requests
+    let mut core = Core::new().unwrap();
+    let client_config: hyper::client::Config<_, _> = ::hyper::Client::configure().connector(
+        hyper_tls::HttpsConnector::new(4, &core.handle()).unwrap(),
+    );
+    let client = client_config.build(&core.handle());
+
+    // The endpoint for our API request to alpha
+    let uri = format!("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GE&interval=60min&apikey={}", api_key).parse().unwrap();
+    let mut finResult = FinanceData { ..Default::default() };
+
+    // Now let's make the request!
+    let work = client.get(uri).and_then(|res| {
+        println!("Response: {}", res.status());
+        res.body().concat2().and_then(move |body| {
+            let v: Value = serde_json::from_slice(&body)
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+                .unwrap();
+
+            // time interval wanted
+            let series = "Time Series (60min)";
+
+            // get the time of last k
+            let mut time = serde_json::to_string(&v["Meta Data"]["3. Last Refreshed"]).unwrap();
+            time.remove(0);
+            time.pop();
+            println!("{}", time);
+            finResult.time = time.clone();
+
+            //open of last refreshed
+            let mut open = serde_json::to_string(&v[series][&time]["1. open"]).unwrap();
+            open.remove(0);
+            open.pop();
+            println!("{}", open);
+            let openfloat: f64 = open.parse().unwrap();
+            println!("{}", openfloat);
+            finResult.open = openfloat;
+
+            //high of last refreshed
+            let mut high = serde_json::to_string(&v[series][&time]["2. high"]).unwrap();
+            high.remove(0);
+            high.pop();
+            println!("{}", high);
+            let highfloat: f64 = high.parse().unwrap();
+            println!("{}", highfloat);
+            finResult.hr_high = highfloat;
+
+            //low of last refreshed
+            let mut low = serde_json::to_string(&v[series][&time]["3. low"]).unwrap();
+            low.remove(0);
+            low.pop();
+            println!("{}", low);
+            let lowfloat: f64 = low.parse().unwrap();
+            println!("{}", openfloat);
+            finResult.hr_low = lowfloat;
+
+            //close of last refreshed
+            let mut close = serde_json::to_string(&v[series][&time]["4. close"]).unwrap();
+            close.remove(0);
+            close.pop();
+            println!("{}", close);
+            let closefloat: f64 = close.parse().unwrap();
+            println!("{}", closefloat);
+            finResult.close = closefloat;
+
+            //volume of last refreshed
+            let mut volume = serde_json::to_string(&v[series][&time]["5. volume"]).unwrap();
+            volume.remove(0);
+            volume.pop();
+            println!("{}", volume);
+            let volumefloat: f64 = volume.parse().unwrap();
+            println!("{}", volumefloat);
+            finResult.hr_volume = volumefloat;
+            //print checks
+            //println!("{}", v[series][&time]);
+            //println!("{}", v[series][&time]["1. open"]);
+            //println!("{}", v[series][&time]["2. high"]);
+            //println!("{}", v[series][&time]["3. low"]);
+            //println!("{}", v[series][&time]["4. close"]);
+            //println!("{}", v[series][&time]["5. volume"]);
 
 
-fn main(){
-	let mut core = Core::new().unwrap();
-	 let client = ::hyper::Client::configure()
-        .connector(::hyper_tls::HttpsConnector::new(4, &core.handle()).unwrap())
-        .build(&core.handle());
-
-	let uri = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GE&interval=1min&apikey=KPMZYNOVSB3KWEIJ".parse().unwrap();
-	
-	let work = client.get(uri).and_then(|res| {
-		println!("Response: {}", res.status());
-		res.body().concat2().and_then(move |body| {
-			let v: Value = serde_json::from_slice(&body).map_err(|e| {
-				io::Error::new(
-					io::ErrorKind::Other,
-					e
-				)
-			}).unwrap();
-			
-			//time interval wanted
-			let series = "Time Series (1min)";
-			
-			//time of last refreshed
-			let mut time = serde_json::to_string(&v["Meta Data"]["3. Last Refreshed"]).unwrap();
-			time.remove(0);
-			time.pop();
-			println!("{}", time);
-			
-			//open of last refreshed
-			let mut open= serde_json::to_string(&v[series][&time]["1. open"]).unwrap();
-			open.remove(0);
-			open.pop();
-			println!("{}", open);
-			let openfloat: f64 = open.parse().unwrap();
-			println!("{}", openfloat);
-			
-			//high of last refreshed
-			let mut high = serde_json::to_string(&v[series][&time]["2. high"]).unwrap();
-			high.remove(0);
-			high.pop();
-			println!("{}", high);
-			let highfloat: f64 = high.parse().unwrap();
-			println!("{}", highfloat);
-			
-			//low of last refreshed
-			let mut low = serde_json::to_string(&v[series][&time]["3. low"]).unwrap();
-			low.remove(0);
-			low.pop();
-			println!("{}", low);
-			let lowfloat: f64 = low.parse().unwrap();
-			println!("{}", openfloat);
-			
-			//close of last refreshed
-			let mut close = serde_json::to_string(&v[series][&time]["4. close"]).unwrap();
-			close.remove(0);
-			close.pop();
-			println!("{}", close);
-			let closefloat: f64 = close.parse().unwrap();
-			println!("{}", closefloat);
-			
-			//volume of last refreshed
-			let mut volume = serde_json::to_string(&v[series][&time]["5. volume"]).unwrap();
-			volume.remove(0);
-			volume.pop();
-			println!("{}", volume);
-			let volumefloat: f64 = volume.parse().unwrap();
-			println!("{}", volumefloat);
-			
-			//print checks
-			//println!("{}", v[series][&time]);
-			//println!("{}", v[series][&time]["1. open"]);
-			//println!("{}", v[series][&time]["2. high"]);
-			//println!("{}", v[series][&time]["3. low"]);
-			//println!("{}", v[series][&time]["4. close"]);
-			//println!("{}", v[series][&time]["5. volume"]);
-			
-			
-			
-			
-			Ok(())
-		})
-	
-	});
-	core.run(work).unwrap();
 
 
-//api key
-//KPMZYNOVSB3KWEIJ
-//https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=GE&interval=1min&apikey=KPMZYNOVSB3KWEIJ
+            Ok(finResult)
+        })
+    });
 
+
+    return core.run(work).unwrap();
 }
 
 
 
-	//res.body().concat2().and_then(move |body| {
+//res.body().concat2().and_then(move |body| {
 
-		//println!("current IP address is {}", v["origin"]);
-		//Ok(())
-		//})
-		
-		
+//println!("current IP address is {}", v["origin"]);
+//Ok(())
+//})
+
+
 
 //Ok(())
--- src/server.rs
+++ src/server.rs
@@ -25,14 +25,15 @@ use rocket::http::ContentType;
 use rocket::response::Response;
 use rocket::response::content;
 
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
 extern crate serde;
 extern crate serde_json;
 use serde_json::{Value, Error};
 
 enum FeedFormat {
-    Alexa
+    Alexa,
 }
 
 #[derive(Serialize)]
@@ -41,7 +42,7 @@ struct AlexaItem {
     updateDate: String,
     titleText: String,
     mainText: String,
-    redirectionUrl: String
+    redirectionUrl: String,
 }
 
 #[get("/")]
@@ -49,23 +50,22 @@ fn index() -> &'static str {
     "Webpage Under Construction!"
 }
 
-fn get_feed() -> Result<Value, Error> { 
+fn get_feed() -> Result<Value, Error> {
 
     let mut settings = config::Config::default();
     settings
-        .merge(File::with_name("conf/sources.toml")).unwrap();
-    let sources = settings.get::< HashMap<String, String>>("event_registry").unwrap();
+        .merge(File::with_name("conf/sources.toml"))
+        .unwrap();
+    let sources = settings
+        .get::<HashMap<String, String>>("event_registry")
+        .unwrap();
 
     let mut core = Core::new().unwrap();
     let client = Client::new(&core.handle());
     let uri = ("http://eventregistry.org/json/event?query=%7B%22%24query%22%3A%7B%22%24and%22%3A%5B%7B%22conceptUri%22%3A%7B%22%24and%22%3A%5B%22http%3A%2F%2Fen.wikipedia.org%2Fwiki%2FGeneral_Electric%22%5D%7D%7D%2C%7B%22lang%22%3A%22eng%22%7D%5D%7D%7D&action=getEvents&resultType=events&eventsSortBy=rel&eventsCount=20&eventsIncludeEventSummary=true&eventsIncludeEventCategories=false&eventsIncludeEventLocation=false&eventsConceptType=org&eventsIncludeSourceTitle=false&eventsIncludeSourceDetails=true&callback=JSON_CALLBACK"
         .to_string() + &"&apiKey=".to_string() + &sources["api_key"]).parse().unwrap();;
     // let uri = "http://httpbin.org/ip"
-    let work = client
-        .get(uri)
-        .and_then(|res| {
-            res.body().concat2()
-        });
+    let work = client.get(uri).and_then(|res| res.body().concat2());
     let data = core.run(work).unwrap();
 
     let mut response = String::from_utf8(data.to_vec()).unwrap();
@@ -81,11 +81,11 @@ fn get_feed() -> Result<Value, Error> {
 
 fn get_string(s1: String) -> String {
     let s2 = s1.trim_left_matches("\"");
-    let s3 : String = String::from(s2.trim_right_matches("\""));
+    let s3: String = String::from(s2.trim_right_matches("\""));
     return s3;
 }
 
-fn get_sentiment(text : String) -> String {
+fn get_sentiment(text: String) -> String {
     let sent = sentiment::analyze(text);
     if sent.negative.score > sent.positive.score {
         return String::from("negative");
@@ -99,34 +99,39 @@ fn get_sentiment(text : String) -> String {
 fn format_alexa(v: Value) -> Result<String, Error> {
     // println!("Got to formatting.");
     // println!("Received: {}", v);
-    let mut formatted_results : Vec<AlexaItem> = Vec::new();
-    let results : &Vec<Value> = v["events"]["results"].as_array().unwrap();
+    let mut formatted_results: Vec<AlexaItem> = Vec::new();
+    let results: &Vec<Value> = v["events"]["results"].as_array().unwrap();
     let mut i = 0;
-    let ge_id : String = format!("\"{}\"", 312064);
+    let ge_id: String = format!("\"{}\"", 312064);
     for res in results {
         // println!("Look at this! {}", res);
 
-        let concepts : &Vec<Value> = res["concepts"].as_array().unwrap();
+        let concepts: &Vec<Value> = res["concepts"].as_array().unwrap();
         for con in concepts {
-            let con_id : String = con["id"].to_string();
+            let con_id: String = con["id"].to_string();
             if con_id == ge_id && i < 6 {
                 let date = Local::now();
                 let time = date.format("%s");
-                let uid : String = format!("item_num_{}_{}", time, i);
+                let uid: String = format!("item_num_{}_{}", time, i);
                 let temp_title = format!("GE News as of {}.", date.format("%A %B %e, %Y"));
                 // yyyy-MM-dd'T'HH:mm:ss'.0Z'
-                let update_date : String = get_string(res["eventDate"].to_string()) + &String::from("T00:00:00.0Z");
-                let main_text : String = get_string(res["summary"]["eng"].to_string());
+                let update_date: String = get_string(res["eventDate"].to_string()) +
+                    &String::from("T00:00:00.0Z");
+                let main_text: String = get_string(res["summary"]["eng"].to_string());
                 let sent = get_sentiment(main_text.clone());
-                let redirection_url : String = String::from("http://eventregistry.org/event/") + &uid;
-                let num_articles : String = get_string(res["totalArticleCount"].to_string());
-                let title_text : String = num_articles + &String::from(" articles discuss ") + &get_string(res["title"]["eng"].to_string()) + &String::from(". This is perceived as generally ") + &get_string(sent);
+                let redirection_url: String = String::from("http://eventregistry.org/event/") +
+                    &uid;
+                let num_articles: String = get_string(res["totalArticleCount"].to_string());
+                let title_text: String = num_articles + &String::from(" articles discuss ") +
+                    &get_string(res["title"]["eng"].to_string()) +
+                    &String::from(". This is perceived as generally ") +
+                    &get_string(sent);
                 let cur_result = AlexaItem {
                     mainText: title_text,
                     titleText: temp_title,
                     uid: uid,
                     updateDate: update_date,
-                    redirectionUrl: redirection_url
+                    redirectionUrl: redirection_url,
                 };
                 formatted_results.push(cur_result);
                 i += 1;
@@ -137,7 +142,7 @@ fn format_alexa(v: Value) -> Result<String, Error> {
     Ok(serde_json::to_string(&formatted_results)?)
 }
 
-fn build_feed(format:FeedFormat) -> String {
+fn build_feed(format: FeedFormat) -> String {
     let v: Value = get_feed().unwrap();
     match format {
         FeedFormat::Alexa => {
@@ -157,9 +162,22 @@ fn serve_feed(file: String) -> content::Json<String> {
 
 fn main() {
     // println!("{}", build_feed(FeedFormat::Alexa));
-    event::fetch();
-    finance::fetch(String::from("api_key"));
-    analyzer::analyze();
+    // event::fetch();
+
+    let mut settings = config::Config::default();
+    settings
+        .merge(File::with_name("conf/sources.toml"))
+        .unwrap();
+    let alpha_key = settings
+        .get::<HashMap<String, String>>("alpha")
+        .unwrap()
+        .get("api_key")
+        .unwrap()
+        .clone();
+
+    finance::fetch(alpha_key);
+
+    // analyzer::analyze();
 
     rocket::ignite()
         .mount("/", routes![index, serve_feed])
